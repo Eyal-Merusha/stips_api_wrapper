6eee77c293ef0b0d837600611274352d0de100f8
Eyal Gal <170107eyalgal@gmail.com>
2025-04-05 08:35:58 +0300

    Adding return values for the methods and documentation

--- stips_framework.py
+++ stips_framework.py
@@ -24,32 +24,75 @@ class stips:
             self.session.close()
 
     
-    def _make_request(self, params: dict):
+    def _make_request(self, params: dict) -> dict:
+        """
+        Makes a request to Stips with the input parameters and returns a parsed json response
+            
+            Parameters:
+                params: The parameters of the request
+            
+            Returns:
+                The response of the server
+        """
         response = self.session.get(URL, params=params)
 
         print(response.status_code)
         print(response.text)
+
+        return json.loads(response.text)
     
 
-    def send_message(self, user_id : int, message : str):
+    def send_message(self, user_id : int, message : str) -> int:
+        """
+        Sends a message to a user
+
+            Parameters:
+                user_id: The id of the user
+                message: The message to send
+
+            Returns:
+                The id of the message which was sent (0 if failed)
+        """
         params = {
             "name": "messages.send",
             "api_params": json.dumps({"touserid": user_id, "msg": message})}
         
-        self._make_request(params)
+        res = self._make_request(params)
+
+        return res["data"]["newid"]
 
 
-    def login(self, email : str, password : str):
+    def login(self, email : str, password : str) -> bool:
+        """
+        Logs in to a user
+
+            Parameters:
+                email: The email of the user
+                password: The password of the user
+
+            Returns:
+                Whether or not it was successful
+        """
         params = {
             "name": "user.login",
             "api_params": json.dumps({"email": email, "password": password, "auth_token": ""})}
         
-        self._make_request(params)
+        res = self._make_request(params)
+        
+        return res["data"]["logged"]
 
 
-    def logout(self):
+    def logout(self) -> bool:
+        """
+        Logs out
+
+            Returns:
+                Whether or not it was successful
+        """
         params = {
             "name": "user.logout",
             "api_params": json.dumps({})}
         
-        self._make_request(params)
+        res = self._make_request(params)
+
+        return res["data"]["success"]
